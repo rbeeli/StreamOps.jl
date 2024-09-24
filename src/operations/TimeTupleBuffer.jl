dd58ee37bcc63ecb5c0a3a47523faffb96b032d2
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2024-09-24 02:11:23 +0200

    added TimeTupleBuffer

---
+++ src/operations/TimeTupleBuffer.jl
@@ -0,0 +1,37 @@
+"""
+Buffer operation that stores all values in a vector.
+
+A storage vector can be provided to the constructor to
+use an existing vector as the underlying storage.
+
+A minimum count can be specified which marks
+this operation as valid only when the buffer has at least
+`min_count` many values.
+
+# Arguments
+- `min_count=0`: The minimum number of values required in the buffer to be considered valid.
+- `storage=T[]`: Use provided vector for storage instead of creating a new one.
+"""
+mutable struct TimeTupleBuffer{TTime,TValue} <: StreamOperation
+    const buffer::Vector{Tuple{TTime,TValue}}
+    const min_count::Int
+    
+    function TimeTupleBuffer{TTime,TValue}(; min_count=0) where {TTime,TValue}
+        new{TTime,TValue}(Tuple{TTime,TValue}[], min_count)
+    end
+
+    function TimeTupleBuffer(storage::Vector{Tuple{TTime,TValue}}; min_count=0) where {TTime,TValue}
+        new{TTime,TValue}(storage, min_count)
+    end
+end
+
+@inline function (op::TimeTupleBuffer{TTime,TValue})(executor, val::TValue) where {TTime,TValue}
+    push!(op.buffer, (time(executor), val))
+    nothing
+end
+
+@inline is_valid(op::TimeTupleBuffer) = length(op.buffer) >= op.min_count
+
+@inline get_state(op::TimeTupleBuffer) = op.buffer
+
+@inline Base.empty!(op::TimeTupleBuffer) = empty!(op.buffer)
---
+++ test/operations/TimeTupleBuffer.jl
@@ -0,0 +1,114 @@
+using Test
+using StreamOps
+
+@testset verbose = true "TimeTupleBuffer" begin
+
+    @testset "default" begin
+        g = StreamGraph()
+
+        values = source!(g, :values, out=Int, init=0)
+        buffer = sink!(g, :buffer, TimeTupleBuffer{DateTime,Int}())
+
+        @test buffer.operation.min_count == 0
+
+        bind!(g, values, buffer)
+
+        exe = compile_historic_executor(DateTime, g; debug=!true)
+        input = [
+            (DateTime(2000, 1, 1), 1),
+            (DateTime(2000, 1, 2), 2),
+            (DateTime(2000, 1, 3), 3),
+            (DateTime(2000, 1, 4), 4)
+        ]
+        start = DateTime(2000, 1, 1)
+        stop = DateTime(2000, 1, 4)
+        adapters = [
+            IterableAdapter(exe, values, input)
+        ]
+        run_simulation!(exe, adapters, start, stop)
+
+        actual = get_state(buffer.operation)
+        @test length(actual) == 4
+        @test all(actual .== input)
+    end
+
+    @testset "min_count" begin
+        g = StreamGraph()
+
+        values = source!(g, :values, out=Int, init=0)
+        buffer = op!(g, :buffer, TimeTupleBuffer{DateTime,Int}(min_count=3), out=Vector{Tuple{DateTime,Int}})
+        output = sink!(g, :output, Counter())
+
+        @test buffer.operation.min_count == 3
+
+        bind!(g, values, buffer)
+        bind!(g, buffer, output)
+
+        exe = compile_historic_executor(DateTime, g; debug=!true)
+        input = [
+            (DateTime(2000, 1, 1), 1),
+            (DateTime(2000, 1, 2), 2),
+            (DateTime(2000, 1, 3), 3),
+            (DateTime(2000, 1, 4), 4)
+        ]
+        start = DateTime(2000, 1, 1)
+        stop = DateTime(2000, 1, 4)
+        adapters = [
+            IterableAdapter(exe, values, input)
+        ]
+        run_simulation!(exe, adapters, start, stop)
+        
+        # output should only be called twice because of min_count=3
+        @test get_state(output.operation) == 2
+    end
+
+    @testset "w/ flush" begin
+        g = StreamGraph()
+
+        # Create source nodes
+        timer = source!(g, :timer, out=DateTime, init=DateTime(0))
+        values = source!(g, :values, out=Float64, init=0.0)
+
+        # Create operation nodes
+        buffer = op!(g, :buffer, TimeTupleBuffer{DateTime,Float64}(), out=TimeTupleBuffer{DateTime,Float64})
+        flush_buffer = op!(g, :flush_buffer, Func{Vector{Tuple{DateTime,Float64}}}((exe, buf, dt) -> begin
+                    vals = copy(buf)
+                    empty!(buf)
+                    vals
+                end, Tuple{DateTime,Float64}[]), out=Vector{Tuple{DateTime,Float64}})
+
+        @test buffer.operation.min_count == 0
+
+        # Create sink nodes
+        collected = []
+        output = sink!(g, :output, Func((exe, x) -> push!(collected, collect(x)), nothing))
+
+        # Create edges between nodes (define the computation graph)
+        bind!(g, values, buffer)
+        bind!(g, buffer, flush_buffer; call_policies=[Never()])
+        bind!(g, timer, flush_buffer)
+        bind!(g, flush_buffer, output)
+
+        exe = compile_historic_executor(DateTime, g; debug=!true)
+        input = [
+            (DateTime(2000, 1, 1), 1.0),
+            (DateTime(2000, 1, 2), 2.0),
+            (DateTime(2000, 1, 3), 3.0),
+            (DateTime(2000, 1, 4), 4.0),
+            (DateTime(2000, 1, 5), 5.0),
+            (DateTime(2000, 1, 6), 6.0)
+        ]
+        start = DateTime(2000, 1, 1)
+        stop = DateTime(2000, 1, 6)
+        adapters = [
+            TimerAdapter{DateTime}(exe, timer; interval=Dates.Day(2), start_time=start),
+            IterableAdapter(exe, values, input)
+        ]
+        run_simulation!(exe, adapters, start, stop)
+        @test collected[1] == Tuple{DateTime,Float64}[]
+        @test collected[2] == input[1:2]
+        @test collected[3] == input[3:4]
+        @test length(collected) == 3
+    end
+
+end
