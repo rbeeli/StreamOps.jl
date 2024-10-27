a97e9a8f937ba3670b96cd5901d65f06278e256b
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2024-10-28 00:06:23 +0100

    optional min_count argument for Counter

--- src/statistics/Counter.jl
+++ src/statistics/Counter.jl
@@ -1,11 +1,18 @@
 """
 A counter of type `T` that increments by one each time it is called.
+
+Optionally, a `min_count` value can be specified from when onwards
+this operation changes into the "valid" state. This is useful if
+other operations need to first be called multiple times before downstream
+operations can start processing, so this operation can be used to signal when
+these operations are deemed ready and further processing can begin.
 """
 mutable struct Counter{T} <: StreamOperation
     counter::T
+    const min_count::T
     
-    Counter(start::T=0) where{T} = new{T}(start)
-    Counter{T}() where{T} = new{T}(zero(T))
+    Counter(start::T=0; min_count::T=0) where{T} = new{T}(start, min_count)
+    Counter{T}(; min_count::T=0) where{T} = new{T}(zero(T), min_count)
 end
 
 @inline function(op::Counter{T})(args...) where {T}
@@ -13,7 +20,7 @@ end
     nothing
 end
 
-@inline is_valid(op::Counter) = true
+@inline is_valid(op::Counter) = op.counter >= op.min_count
 
 @inline function get_state(op::Counter{T})::T where {T}
     op.counter
--- test/statistics/Counter.jl
+++ test/statistics/Counter.jl
@@ -1,11 +1,11 @@
-@testitem "Counter" begin
+@testitem "Counter (incl. reset!)" begin
     using Dates
     
     g = StreamGraph()
 
-    timer = source!(g, :timer, out=DateTime, init=DateTime(0))
-    counter = sink!(g, :counter, Counter())
-    bind!(g, timer, counter)
+    source!(g, :timer, out=DateTime, init=DateTime(0))
+    sink!(g, :counter, Counter())
+    bind!(g, :timer, :counter)
 
     states = compile_graph!(DateTime, g)
     exe = HistoricExecutor{DateTime}(g, states)
@@ -14,11 +14,40 @@
     start = DateTime(2000, 1, 1, 0, 0, 0)
     stop = DateTime(2000, 1, 1, 0, 0, 15)
     set_adapters!(exe, [
-        HistoricTimer{DateTime}(exe, timer; interval=Second(5), start_time=start),
+        HistoricTimer{DateTime}(exe, g[:timer], interval=Second(5), start_time=start),
     ])
     run!(exe, start, stop)
-    @test get_state(counter.operation) == 4 # 0, 5, 10, 15
 
-    reset!(counter.operation)
-    @test get_state(counter.operation) == 0
+    @test get_state(g[:counter].operation) == 4 # 0, 5, 10, 15
+
+    reset!(g[:counter].operation)
+    @test get_state(g[:counter].operation) == 0
+end
+
+@testitem "Counter min_count" begin
+    using Dates
+    
+    g = StreamGraph()
+
+    source!(g, :timer, out=DateTime, init=DateTime(0))
+    op!(g, :counter, Counter(min_count=3), out=Int)
+    bind!(g, :timer, :counter)
+    sink!(g, :output, Buffer{Int}())
+    bind!(g, :counter, :output)
+
+    states = compile_graph!(DateTime, g)
+    exe = HistoricExecutor{DateTime}(g, states)
+    setup!(exe)
+
+    start = DateTime(2000, 1, 1)
+    stop = DateTime(2000, 1, 15)
+    set_adapters!(exe, [
+        HistoricTimer{DateTime}(exe, g[:timer], interval=Day(1), start_time=start),
+    ])
+    run!(exe, start, stop)
+
+    @test get_state(g[:counter].operation) == 15
+
+    # from 3 onwards it should be valid and start collecting
+    @test all(get_state(g[:output].operation) .== 3:15)
 end
