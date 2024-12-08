1fe11d6603ab791b1da55835f4496cc98954b0de
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2024-12-08 06:11:13 +0100

    0.6.2: Fix duplicate call policy constructors

--- src/graph/call_policies.jl
+++ src/graph/call_policies.jl
@@ -18,16 +18,22 @@ end
 struct IfExecuted <: CallPolicy
     nodes::Union{Symbol,Vector{Symbol}} # :any, :all or node label symbols
 
-    function IfExecuted(mode::Symbol=:all)
-        mode ∈ (:any, :all) && return new(mode)
-        new([mode])
+    # Primary constructor
+    function IfExecuted(node::Union{Symbol,String}=:all)
+        if node isa String
+            node = Symbol(node)
+        end
+        node ∈ (:any, :all) && return new(node)
+        new([node])
     end
-    IfExecuted(nodes::Symbol...) = new(collect(nodes))
+
+    function IfExecuted(nodes::Union{Tuple{Vararg{T}},Vector{T}}) where {T<:Union{Symbol,String}}
+        new([Symbol(x) for x in nodes])
+    end
 end
 
-# constructor String support
-IfExecuted(mode::String="all") = IfExecuted(Symbol(mode))
-IfExecuted(nodes::String...) = IfExecuted((Symbol(node) for node in nodes)...)
+# Outer constructors
+IfExecuted(nodes::Union{Symbol,String}...) = IfExecuted(nodes)
 
 
 # # If connected node was NOT executed, REGARDLESS of valid output or not, trigger the node
@@ -45,32 +51,44 @@ IfExecuted(nodes::String...) = IfExecuted((Symbol(node) for node in nodes)...)
 struct IfValid <: CallPolicy
     nodes::Union{Symbol,Vector{Symbol}} # :any, :all or node label symbols
 
-    function IfValid(mode::Symbol=:all)
-        mode ∈ (:any, :all) && return new(mode)
-        new([mode])
+    # Primary constructor
+    function IfValid(node::Union{Symbol,String}=:all)
+        if node isa String
+            node = Symbol(node)
+        end
+        node ∈ (:any, :all) && return new(node)
+        new([node])
     end
-    IfValid(nodes::Symbol...) = new(collect(nodes))
+
+    function IfValid(nodes::Union{Tuple{Vararg{T}},Vector{T}}) where {T<:Union{Symbol,String}}
+        new([Symbol(x) for x in nodes])
+    end
 end
 
-# constructor String support
-IfValid(mode::String="all") = IfValid(Symbol(mode))
-IfValid(nodes::String...) = IfValid((Symbol(node) for node in nodes)...)
+# Outer constructors
+IfValid(nodes::Union{Symbol,String}...) = IfValid(nodes)
 
 
 # If connected node(s) have invalid output(s), trigger the node
 struct IfInvalid <: CallPolicy
     nodes::Union{Symbol,Vector{Symbol}} # :any, :all or node label symbols
 
-    function IfInvalid(mode::Symbol=:any)
-        mode ∈ (:any, :all) && return new(mode)
-        new([mode])
+    # Primary constructor
+    function IfInvalid(node::Union{Symbol,String}=:any)
+        if node isa String
+            node = Symbol(node)
+        end
+        node ∈ (:any, :all) && return new(node)
+        new([node])
     end
-    IfInvalid(nodes::Symbol...) = new(collect(nodes))
+
+    function IfInvalid(nodes::Union{Tuple{Vararg{T}},Vector{T}}) where {T<:Union{Symbol,String}}
+        new([Symbol(x) for x in nodes])
+    end
 end
 
-# constructor String support
-IfInvalid(mode::String="any") = IfInvalid(Symbol(mode))
-IfInvalid(nodes::String...) = IfInvalid((Symbol(node) for node in nodes)...)
+# Outer constructors
+IfInvalid(nodes::Union{Symbol,String}...) = IfInvalid(nodes)
 
 
 function _make_label(nodes::Union{Symbol,Vector{Symbol}})
